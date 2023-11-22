23fedc782f808c3ec52feef36f9ad6743f3abe44
Ozgur32 <odemirbacak4985@gmail.com>
2023-11-22 11:29:58 +0300

    deploy

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -5,7 +5,8 @@ import { File } from 'lucide-react'
 import { useQuery } from 'convex/react'
 import { useRouter } from 'next/navigation'
 import { useUser } from '@clerk/clerk-react'
-import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './ui/command'
+
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
 import { useSearch } from '@/hooks/use-search'
 import { api } from '@/convex/_generated/api'
 
@@ -13,11 +14,11 @@ export const SearchCommand = () => {
     const { user } = useUser()
     const router = useRouter()
     const documents = useQuery(api.documents.getSearch)
+    const [isMounted, setIsMounted] = useState(false)
+
     const toggle = useSearch((store) => store.toggle)
     const isOpen = useSearch((store) => store.isOpen)
     const onClose = useSearch((store) => store.onClose)
-
-    const [isMounted, setIsMounted] = useState(false)
 
     useEffect(() => {
         setIsMounted(true)
@@ -32,7 +33,6 @@ export const SearchCommand = () => {
         }
 
         document.addEventListener('keydown', down)
-
         return () => document.removeEventListener('keydown', down)
     }, [toggle])
 
@@ -41,16 +41,18 @@ export const SearchCommand = () => {
         onClose()
     }
 
-    if (!isMounted) return null
+    if (!isMounted) {
+        return null
+    }
 
     return (
         <CommandDialog open={isOpen} onOpenChange={onClose}>
-            <CommandInput placeholder={`Search ${user?.fullName}'s Notion...`} />
+            <CommandInput placeholder={`Search ${user?.fullName}'s Jotion...`} />
             <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
                 <CommandGroup heading="Documents">
                     {documents?.map((document) => (
-                        <CommandItem key={document._id} value={`${document._id}-${document.title}`} title={document.title} onSelect={onSelect}>
+                        <CommandItem key={document._id} value={`${document._id}-${document.title}`} title={document.title} onSelect={() => onSelect(document._id)}>
                             {document.icon ? <p className="mr-2 text-[18px]">{document.icon}</p> : <File className="mr-2 h-4 w-4" />}
                             <span>{document.title}</span>
                         </CommandItem>
--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,24 +1,23 @@
 'use client'
 
-import { Doc } from '@/convex/_generated/dataModel'
-import IconPicker from './icon-picker'
-import { Button } from './ui/button'
+import { ElementRef, useRef, useState } from 'react'
 import { ImageIcon, Smile, X } from 'lucide-react'
-import React, { ElementRef, useRef, useState } from 'react'
 import { useMutation } from 'convex/react'
-import { api } from '@/convex/_generated/api'
-import TextAreaAutosize from 'react-textarea-autosize'
+import TextareaAutosize from 'react-textarea-autosize'
 import { useCoverImage } from '@/hooks/use-cover-image'
+import { Doc } from '@/convex/_generated/dataModel'
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import IconPicker from './icon-picker'
 
 interface ToolbarProps {
     initialData: Doc<'documents'>
     preview?: boolean
 }
 
-const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
     const inputRef = useRef<ElementRef<'textarea'>>(null)
     const [isEditing, setIsEditing] = useState(false)
-
     const [value, setValue] = useState(initialData.title)
 
     const update = useMutation(api.documents.update)
@@ -30,7 +29,6 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
         if (preview) return
 
         setIsEditing(true)
-
         setTimeout(() => {
             setValue(initialData.title)
             inputRef.current?.focus()
@@ -47,9 +45,9 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
         })
     }
 
-    const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === 'Enter') {
-            e.preventDefault()
+    const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter') {
+            event.preventDefault()
             disableInput()
         }
     }
@@ -82,36 +80,34 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
             {!!initialData.icon && preview && <p className="text-6xl pt-6">{initialData.icon}</p>}
             <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
                 {!initialData.icon && !preview && (
-                    <IconPicker onChange={onIconSelect} asChild>
-                        <Button className="text-muted-foreground text-xs " variant="outline" size="sm">
+                    <IconPicker asChild onChange={onIconSelect}>
+                        <Button className="text-muted-foreground text-xs" variant="outline" size="sm">
                             <Smile className="h-4 w-4 mr-2" />
                             Add icon
                         </Button>
                     </IconPicker>
                 )}
                 {!initialData.coverImage && !preview && (
-                    <Button className="text-muted-foreground text-xs " variant="outline" size="sm" onClick={coverImage.onOpen}>
-                        <ImageIcon className="h-4 w-4 mr-2 " />
+                    <Button onClick={coverImage.onOpen} className="text-muted-foreground text-xs" variant="outline" size="sm">
+                        <ImageIcon className="h-4 w-4 mr-2" />
                         Add cover
                     </Button>
                 )}
             </div>
             {isEditing && !preview ? (
-                <TextAreaAutosize
+                <TextareaAutosize
                     ref={inputRef}
                     onBlur={disableInput}
                     onKeyDown={onKeyDown}
                     value={value}
                     onChange={(e) => onInput(e.target.value)}
-                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3f3f3f] dark:text-[#cfcfcf] resize-none"
+                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
                 />
             ) : (
-                <div onClick={enableInput} className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3f3f3f] dark:text-[#cfcfcf]">
+                <div onClick={enableInput} className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]">
                     {initialData.title}
                 </div>
             )}
         </div>
     )
 }
-
-export default Toolbar
